1e3820b8dd64f9bc7aebf55066c7c2a7e3a0c054
Brazzini OC <bocsagitario@gmail.com>
2023-01-17 00:10:22 -0500

    add email validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -20,30 +20,38 @@ const passwordRecoveryForm = (req, res) => {
 }
 
 const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    const passwordConfirm = req.body.password_confirm;
 
     // validación
     await check('name').notEmpty().withMessage('El Nombre es requerido').run(req);
     await check('email').isEmail().withMessage('Eso no parece un email').run(req);
     await check('password').isLength({ min: 6 }).withMessage('El password debe ser de al menos 6 caracteres').run(req);
-    await check('password_confirm').equals('password').withMessage('Los passwords no son iguales').run(req);
+    await check('password_confirm').equals(req.body.password).withMessage('Los passwords no son iguales').run(req);
 
     let result = validationResult(req);
 
     if (!result.isEmpty()){
         return res.render('auth/register', {
             pageTitle: 'Crear cuenta',
-            data: {
-                name: req.body.name, 
-                email: req.body.email
-            },
+            data: { name, email },
             errors: result.array(),
         });
     }
+  
+    const userAlreadyExist = await User.findOne( { where: { email } });
 
-    const user = await User.create(req.body);
+    if (userAlreadyExist) {
+        return res.render('auth/register', {
+            pageTitle: 'Crear cuenta',
+            data: { name, email },
+            errors: [ { msg: "Correo ya encuentra en uso" }],
+        });
+    }
+
+    const user = await User.create({ name, email, password });
     res.json(user);
 }
-
 
 // export default solo uno puedes tener uno solo por archivo
 export {
